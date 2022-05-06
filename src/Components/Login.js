a15e61a93bb9e374163bcc3145780c69dbadc057
Divviya <85920511+DivviyaC@users.noreply.github.com>
2022-05-06 10:28:45 +0530

    update colors

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -149,43 +149,43 @@ function Login() {
   return (
     <div className="container-fluid Login_info pt-2">
 
-    <div className="row pt-5">
-    <div className="col-lg-4 col-md-4 col-12 pt-lg-0 pt-md-0 pt-5">
-            <section className="d-flex justify-content-center flex-column align-items-center">
+      <div className="row pt-5">
+        <div className="col-lg-4 col-md-4 col-12 pt-lg-0 pt-md-0 pt-5">
+          <section className="d-flex justify-content-center flex-column align-items-center">
 
-              <img src={logo2} className="img-fluid" width={100} />
-              <img src={logo3} className="img-fluid py-2" width={300} />
-            <label> V.2.1.2 </label>
-    <input type="textInput"
-      placeholder="USER ID"
-      className=" my-2 login_input form-control w-75 w-sm-50"
-      onChange={(u) => setUserName(u.target.value)}
-    />
+            <img src={logo2} className="img-fluid" width={100} />
+            <img src={logo3} className="img-fluid py-2" width={300} />
+            {/* <label> V.2.1.2 </label> */}
+            <input type="textInput"
+              placeholder="USER ID"
+              className=" my-2 login_input form-control w-75 w-sm-50"
+              onChange={(u) => setUserName(u.target.value)}
+            />
 
-    <input type="password"
-      placeholder="PASSWORD"
-      className=" my-2 login_input form-control w-75 w-sm-50"
-      // secureTextEntry={true}
-      onChange={(password) => setPassword(password.target.value)}
-    // onSubmitEditing={() => submit()}
-    />
+            <input type="password"
+              placeholder="PASSWORD"
+              className=" my-2 login_input form-control w-75 w-sm-50"
+              // secureTextEntry={true}
+              onChange={(password) => setPassword(password.target.value)}
+            // onSubmitEditing={() => submit()}
+            />
 
 
-    <button onClick={() => { submit() }}
-    className="btn btn-success w-75 login_btn"
-    >LOGIN</button>
-    {LoginValidation ? <>Invalid username or password!</>
-      : ""}
-            </section>
+            <button onClick={() => { submit() }}
+              className="btn btn-success w-75 login_btn"
+            >LOGIN</button>
+            {LoginValidation ? <>Invalid username or password!</>
+              : ""}
+          </section>
+        </div>
+        <div className="col-lg-8 col-md-8 col-9 pt-lg-0 pt-md-0 pt-5">
+          <img src={logo} width={900} className="img-fluid " />
+        </div>
+      </div>
+      {/* <image source={require("../")} ></image> */}
+      {/* <image source={require("../images/Pasas-horizontal.png")} /> */}
+
     </div>
-    <div className="col-lg-8 col-md-8 col-9 pt-lg-0 pt-md-0 pt-5">
-      <img src={logo} width={900}  className="img-fluid " />
-    </div>
-    </div>
-    {/* <image source={require("../")} ></image> */}
-    {/* <image source={require("../images/Pasas-horizontal.png")} /> */}
-  
-  </div>
 
 
   );
